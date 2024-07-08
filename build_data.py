c84a96e566f9b8249e359216b7d97a37f0e71248
Ryan LaLiberty <rycolos@gkserver.net>
2024-07-08 09:39:47 -0400

    build_data completed

--- build_data.py
+++ build_data.py
@@ -12,6 +12,55 @@ sclass_details = (
         "tam": 40,
         "armor_roll": "3+",
         "mdpa": 12
+    },
+    {
+        "sclass": "HEAVY DESTROYER",
+        "size": "Medium",
+        "tam": 50,
+        "armor_roll": "4+",
+        "mdpa": 15
+    },
+    {
+        "sclass": "LIGHT CRUISER",
+        "size": "Medium",
+        "tam": 60,
+        "armor_roll": "4+",
+        "mdpa": 18
+    },
+    {
+        "sclass": "CRUISER",
+        "size": "Large",
+        "tam": 70,
+        "armor_roll": "4+",
+        "mdpa": 21
+    },
+    {
+        "sclass": "BATTLECRUISER",
+        "size": "Large",
+        "tam": 90,
+        "armor_roll": "5+",
+        "mdpa": 27
+    },
+    {
+        "sclass": "BATTLESHIP",
+        "size": "Large",
+        "tam": 110,
+        "armor_roll": "5+",
+        "mdpa": 33
+    },
+    {
+        "sclass": "DREADNOUGHT",
+        "size": "Large",
+        "tam": 130,
+        "armor_roll": "5+",
+        "mdpa": 39
+    },
+    {
+        "sclass": "SUPER DREADNOUGHT",
+        "size": "Large",
+        "tam": 150,
+        "armor_roll": "5+",
+        "mdpa": 45
     }
 )
 
@@ -36,6 +85,27 @@ equipment_details = (
         "mass_factor": .05,
         "pv_factor": 9,
         "description": "No minimum 1 hex forward move is required before making a facing change"
+    },
+    {
+        "id": 4,
+        "name": "Enhanced Engineering",
+        "mass_factor": .05,
+        "pv_factor": 10,
+        "description": "Apply a -1 modifier on Damage Control rolls"
+    },
+    {
+        "id": 5,
+        "name": "Advanced Fire Control",
+        "mass_factor": .05,
+        "pv_factor": 10,
+        "description": "Ignore To Hit modifiers for targeting additional ships"
+    },
+    {
+        "id": 6,
+        "name": "Target Designator",
+        "mass_factor": .04,
+        "pv_factor": 12,
+        "description": "After this ship has successfully attacked a target, friendly ships that attack the same target get -1 on their To Hit rolls against that target on the same turn. Can only be used against one target per turn, per ship with this equipment"
     }
 )
 
@@ -126,6 +196,155 @@ weapon_details = (
         "damage": 2,
         "range": "8/14/20/25",
         "special": None
+    },
+    {
+        "name": "HEAVY LASER",
+        "max_dmg": 6,
+        "mass": 5,
+        "pv": 15,
+        "attack": 3,
+        "damage": 3,
+        "range": "9/15/21/27",
+        "special": None
+    },
+    {
+        "name": "PULSE LASER",
+        "max_dmg": 8,
+        "mass": 12,
+        "pv": 36,
+        "attack": 5,
+        "damage": 2,
+        "range": "8/14/20/25",
+        "special": "RPT"
+    },
+    {
+        "name": "LIGHT PPC",
+        "max_dmg": 3,
+        "mass": 4,
+        "pv": 12,
+        "attack": 4,
+        "damage": 1,
+        "range": "8/13/18/23",
+        "special": "PEN1"
+    },
+    {
+        "name": "MEDIUM PPC",
+        "max_dmg": 6,
+        "mass": 9,
+        "pv": 27,
+        "attack": 4,
+        "damage": 2,
+        "range": "8/14/20/25",
+        "special": "PEN2"
+    },
+    {
+        "name": "HEAVY PPC",
+        "max_dmg": 9,
+        "mass": 13,
+        "pv": 39,
+        "attack": 4,
+        "damage": 3,
+        "range": "9/15/21/27",
+        "special": "PEN3"
+    },
+    {
+        "name": "LIGHT NAVEL AUTOCANNON",
+        "max_dmg": 8,
+        "mass": 12,
+        "pv": 36,
+        "attack": 5,
+        "damage": 2,
+        "range": "8/13/18/23",
+        "special": "RPT"
+    },
+    {
+        "name": "MEDIUM NAVEL AUTOCANNON",
+        "max_dmg": 12,
+        "mass": 15,
+        "pv": 45,
+        "attack": 5,
+        "damage": 3,
+        "range": "8/11/16/21",
+        "special": "RPT"
+    },
+    {
+        "name": "HEAVY NAVEL AUTOCANNON",
+        "max_dmg": 16,
+        "mass": 18,
+        "pv": 54,
+        "attack": 5,
+        "damage": 4,
+        "range": "7/11/15/19",
+        "special": "RPT"
+    },
+    {
+        "name": "LIGHT GAUSS",
+        "max_dmg": 12,
+        "mass": 13,
+        "pv": 39,
+        "attack": 5,
+        "damage": 3,
+        "range": "8/14/20/25",
+        "special": "PEN2"
+    },
+    {
+        "name": "MEDIUM GAUSS",
+        "max_dmg": 16,
+        "mass": 18,
+        "pv": 54,
+        "attack": 5,
+        "damage": 4,
+        "range": "8/13/18/23",
+        "special": "PEN3"
+    },
+    {
+        "name": "HEAVY GAUSS",
+        "max_dmg": 20,
+        "mass": 23,
+        "pv": 69,
+        "attack": 5,
+        "damage": 5,
+        "range": "8/11/16/21",
+        "special": "PEN4"
+    },
+    {
+        "name": "NAVAL MASS DRIVER",
+        "max_dmg": 28,
+        "mass": 42,
+        "pv": 126,
+        "attack": 3,
+        "damage": 15,
+        "range": "7/10/14/17",
+        "special": "AM2, MASS"
+    },
+    {
+        "name": "MISSILE CLUSTER - SMALL",
+        "max_dmg": 10,
+        "mass": 8,
+        "pv": 24,
+        "attack": 6,
+        "damage": 2,
+        "range": "8/14/20/25",
+        "special": "SCT, AM4"
+    },
+    {
+        "name": "MISSILE CLUSTER - LARGE",
+        "max_dmg": 15,
+        "mass": 12,
+        "pv": 36,
+        "attack": 6,
+        "damage": 3,
+        "range": "9/15/21/27",
+        "special": "SCT, AM4"
+    },
+    {
+        "name": "MK49 GUIDED TORPEDO",
+        "max_dmg": 12,
+        "mass": 10,
+        "pv": 30,
+        "attack": 1,
+        "damage": 12,
+        "range": "9/15/21/27",
+        "special": "GUI, AM4"
     }
-
 )

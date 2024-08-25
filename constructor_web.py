86a2548309ceddcad483d49a568ab6e2b7da65f4
Ryan LaLiberty <rycolos@gkserver.net>
2024-08-25 07:27:17 -0400

    weapon selection

--- constructor_web.py
+++ constructor_web.py
@@ -39,7 +39,7 @@ if main_selected == 'Build Ship':
     build_col1, build_col2, build_col3 = st.columns(3)
 
 
-    with build_col1.expander('Ship Core', expanded=True):
+    with build_col1.expander('**Ship Core**', expanded=True):
         name = st.text_input("Ship Name")
         sclass = st.selectbox('Ship Class', [s['sclass'] for s in build_data.sclass_details])
         if sclass:
@@ -47,7 +47,7 @@ if main_selected == 'Build Ship':
             if "ship" not in st.session_state:
                 st.session_state.ship = ship
 
-    with build_col1.expander('Armor', expanded=True):
+    with build_col1.expander('**Armor**', expanded=True):
         ohs_input = st.selectbox('Outer Hull Strength', [s['name'] for s in build_data.outer_strength_details])
         if ohs_input:
             ohs_int = [s['id'] for s in build_data.outer_strength_details if s['name'] == ohs_input]
@@ -62,46 +62,58 @@ if main_selected == 'Build Ship':
             ship.track_mass()
             ship.track_base_pv()
 
-    with build_col1.expander('Propulsion', expanded=True):
+    with build_col1.expander('**Propulsion**', expanded=True):
         tp_input = st.number_input('Thrust Points', min_value=0)
         if tp_input:
             ship.propulsion(tp_input)
             ship.track_mass()
             ship.track_base_pv()
 
-
-    with build_col2.expander('Weapons', expanded=True):
+    with build_col2.expander('**Weapons**', expanded=True):
         front_arc_input = st.multiselect('Front Arc Weapons', [s['name'] for s in build_data.weapon_details])
         if front_arc_input:
-            pass
+            ship.front_arc_weapons(*front_arc_input)
+            st.markdown(f'**Total Front Arc Mass:** {ship.total_front_arc_mass}, **Total Front Arc PV:** {ship.total_front_arc_pv}')
+            ship.track_mass()
+            ship.track_base_pv()
 
         rear_arc_input = st.multiselect('Rear Arc Weapons', [s['name'] for s in build_data.weapon_details])
         if rear_arc_input:
-            pass
+            ship.rear_arc_weapons(*rear_arc_input)
+            st.markdown(f'**Total Rear Arc Mass:** {ship.total_rear_arc_mass}, **Total Rear Arc PV:** {ship.total_rear_arc_pv}')
+            ship.track_mass()
+            ship.track_base_pv()
 
         right_arc_input = st.multiselect('Right Arc Weapons', [s['name'] for s in build_data.weapon_details])
         if right_arc_input:
-            pass
+            ship.right_arc_weapons(*right_arc_input)
+            st.markdown(f'**Total Right Arc Mass:** {ship.total_right_arc_mass}, **Total Right Arc PV:** {ship.total_right_arc_pv}')
+            ship.track_mass()
+            ship.track_base_pv()
 
         left_arc_input = st.multiselect('Left Arc Weapons', [s['name'] for s in build_data.weapon_details])
         if left_arc_input:
-            pass
+            ship.left_arc_weapons(*left_arc_input)
+            st.markdown(f'**Total Left Arc Mass:** {ship.total_left_arc_mass}, **Total Left Arc PV:** {ship.total_left_arc_pv}')
+            ship.track_mass()
+            ship.track_base_pv()
 
-    with build_col2.expander('Equipment', expanded=True):
+    with build_col2.expander('**Equipment**', expanded=True):
         equipment_input = st.multiselect('Equipment', [s['name'] for s in build_data.equipment_details])
         if equipment_input:
             build_col3.write(f'{[s['description'] for s in build_data.equipment_details if s['name'] == equipment_input[-1]][0]}')
-            name_to_id = {item["name"]: item["id"] for item in build_data.equipment_details}
-            equip_int_list = [name_to_id[name] for name in equipment_input]
+            equipment_name_to_id = {item["name"]: item["id"] for item in build_data.equipment_details}
+            equip_int_list = [equipment_name_to_id[name] for name in equipment_input]
             ship.equipment(*equip_int_list)
             st.markdown(f'**Total Equipment Mass:** {ship.total_equipment_mass}, **Total Equipment PV:** {ship.total_equipment_pv}')
             ship.track_mass()
             ship.track_base_pv()
 
-    crew_quality_input = build_col1.selectbox('Crew Quality', [s['name'] for s in build_data.crew_quality_details])
-    if crew_quality_input:
-        crew_quality_int = [s['id'] for s in build_data.crew_quality_details if s['name'] == crew_quality_input]
-        ship.set_quality(crew_quality_int[0])
+    with build_col3.expander('**Crew Quality**', expanded=True):
+        crew_quality_input = st.selectbox('Crew Quality', [s['name'] for s in build_data.crew_quality_details])
+        if crew_quality_input:
+            crew_quality_int = [s['id'] for s in build_data.crew_quality_details if s['name'] == crew_quality_input]
+            ship.set_quality(crew_quality_int[0])
 
     col1, col2, col3, col4 = st.columns(4)
     ship.track_base_pv()
@@ -111,7 +123,7 @@ if main_selected == 'Build Ship':
     col3.metric(label='Base PV', value=ship.total_base_pv)
     col4.metric(label='Final PV', value=ship.final_pv)
 
-    st.write(f"""
+    build_col3.write(f"""
                 **Ship Name:** {ship.name}  
                 **Ship Class:** {ship.sclass}  
                 **Ship Size:** {ship.size}  
@@ -125,10 +137,10 @@ if main_selected == 'Build Ship':
                 **Max Thrust:** {ship.max_thrust}  
                 **Max Stress:** {ship.max_stress}  
                 **Equipment:** {equipment_input}    
-                **Front Arc Weapons:**  
-                **Rear Arc Weapons:**  
-                **Right Arc Weapons:**  
-                **Left Arc Weapons:** 
+                **Front Arc Weapons:**  {front_arc_input}  
+                **Rear Arc Weapons:**  {rear_arc_input}  
+                **Right Arc Weapons:**  {right_arc_input}  
+                **Left Arc Weapons:** {left_arc_input}  
                 """)
 
 st.write(st.session_state)
